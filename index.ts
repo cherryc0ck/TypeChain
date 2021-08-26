3b6fc954672bebb328e2b9c67bd72709b2cccf13
oriharaa <qstar9537@naver.com>
2021-08-26 22:21:00 +0900

    Type in Typescript

--- index.ts
+++ index.ts
@@ -1,11 +1,7 @@
-const name = "HongUnTaek",
-age = 27,
-gender = 'male';
+const sayHi = (name: string, age: number, gender: string): string => {
+  return `Hello ${name}, you are ${age}, you are a ${gender}`;
+};
 
-const sayHi = (name, age, gender?) => {
-  console.log(`Hello ${name}, you are ${age}, you are a ${gender}`);
-}
-const sayHi: (name:any, age: any, gender?: any) => void
-sayHi(name, age);
+console.log(sayHi("Untaek", 27, "male"));
 
-export {};
+export{};
